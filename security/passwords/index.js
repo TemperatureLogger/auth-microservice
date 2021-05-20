a980f1794df65e139d0c69de183e5374ef2bf321
filimonadrian <adrian.filimon@hotmail.com>
2021-05-20 16:36:12 +0300

    Added work with passwords - encode and compare

---
+++ security/passwords/index.js
@@ -0,0 +1,23 @@
+const bcryptjs = require('bcryptjs');
+
+// hash functions --> encrypt the password
+const hash = async (plainTextPass) => {
+    // salt is a random sequence in the front of the encrypted informations
+    // the parameter of genSalt is the strength of the salt
+    const salt = await bcryptjs.genSalt(4);
+    // hash the password with this salt
+    const hash = await bcryptjs.hash(plainTextPass, salt);
+
+    return hash;
+}
+
+// compare hashed password with a plaintext password
+const compare = async (plaintTextPass, hashedPass) => {
+    const isCorrect = await bcryptjs..compare(plaintTextPass, hashedPass);
+    return isCorrect;
+};
+
+module.exports = {
+    hash,
+    compare
+} 
