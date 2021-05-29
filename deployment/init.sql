7f461e7de510023750857d04ec93b88228540b67
filimonadrian <adrian.filimon@hotmail.com>
2021-05-29 15:20:59 +0300

    Added new serial numbers

--- deployment/init.sql
+++ deployment/init.sql
@@ -11,8 +11,28 @@ CREATE TABLE IF NOT EXISTS hwAddress (
     -- userId INTEGER NOT NULL references users(id)
 );
 
+INSERT INTO hwAddress (serialNumber) VALUES (111111);
+INSERT INTO hwAddress (serialNumber) VALUES (111112);
+INSERT INTO hwAddress (serialNumber) VALUES (111113);
+INSERT INTO hwAddress (serialNumber) VALUES (111114);
+INSERT INTO hwAddress (serialNumber) VALUES (111115);
+INSERT INTO hwAddress (serialNumber) VALUES (111116);
+INSERT INTO hwAddress (serialNumber) VALUES (111117);
+INSERT INTO hwAddress (serialNumber) VALUES (111118);
+INSERT INTO hwAddress (serialNumber) VALUES (111119);
+INSERT INTO hwAddress (serialNumber) VALUES (111120);
+INSERT INTO hwAddress (serialNumber) VALUES (111121);
+INSERT INTO hwAddress (serialNumber) VALUES (111122);
+INSERT INTO hwAddress (serialNumber) VALUES (111123);
+INSERT INTO hwAddress (serialNumber) VALUES (111124);
+INSERT INTO hwAddress (serialNumber) VALUES (111125);
+INSERT INTO hwAddress (serialNumber) VALUES (111126);
+INSERT INTO hwAddress (serialNumber) VALUES (111127);
+INSERT INTO hwAddress (serialNumber) VALUES (111128);
+INSERT INTO hwAddress (serialNumber) VALUES (111129);
+
 INSERT INTO hwAddress (serialNumber) VALUES (995346);
 INSERT INTO hwAddress (serialNumber) VALUES (553123);
 INSERT INTO hwAddress (serialNumber) VALUES (123456);
 INSERT INTO hwAddress (serialNumber) VALUES (999999);
-INSERT INTO hwAddress (serialNumber) VALUES (112991);
+INSERT INTO hwAddress (serialNumber) VALUES (112911);
